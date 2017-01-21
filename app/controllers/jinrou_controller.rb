1ffd84d70515a41ed94f9d19b0134d34df5adf61
Masahiro Yamazaki <masahiro777.ymzk@gmail.com>
2017-01-21 12:45:37 +0000

    create outline of select-form

--- app/controllers/jinrou_controller.rb
+++ app/controllers/jinrou_controller.rb
@@ -6,5 +6,9 @@ class JinrouController < ApplicationController
     @day.save!
     @players = Player.all
     @days = Day.all
+    @living_players = Player.where(is_living: 0)
+    @opened_prayers = @living_players.where(opened_role: "占い師")
+    @opened_mystics = @living_players.where(opened_role: "霊媒師")
+    @opened_knights = @living_players.where(opened_role: "騎士")
   end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,3 @@
 class Player < ActiveRecord::Base
+  scope :living, -> { where(is_living: 0) }
 end
