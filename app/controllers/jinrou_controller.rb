d8ce7f0e8a7906d442ae515e832d7a43e7c0fca6
Masahiro Yamazaki <masahiro777.ymzk@gmail.com>
2017-01-21 09:46:48 +0000

    success for form_tag

--- app/controllers/jinrou_controller.rb
+++ app/controllers/jinrou_controller.rb
@@ -1,5 +1,8 @@
 class JinrouController < ApplicationController
   def index
+    day_num = Day.this_day
+    @day = Day.new(day_num: day_num)
+    @day.save!
     @players = Player.all
     @days = Day.all
   end
--- app/models/day.rb
+++ app/models/day.rb
@@ -1,2 +1,6 @@
 class Day < ActiveRecord::Base
+  def self.this_day
+    return 1 if self.all.blank?
+    return (self.all.pluck(:day_num).try(:max) || 0) + 1
+  end
 end
